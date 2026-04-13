3d9e7229fa10a832839ef938930b5d3313e5d97a
Zachary Evans <zb2224@jagmail.southalabama.edu>
2026-04-13 12:35:20 -0500

    Edited for postgres

--- Program.cs
+++ Program.cs
@@ -2,9 +2,9 @@
 using Auth0.AspNetCore.Authentication;
 using MedTrackerScreensMVC.Data;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,8 +17,7 @@ builder.Services.AddAuth0WebAppAuthentication(options =>
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-);
+options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
