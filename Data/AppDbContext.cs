c0a1240c7b7c0eb3ec89e7a4f4f607b58b29dcfb
Zachary Evans <zb2224@jagmail.southalabama.edu>
2026-04-14 12:04:12 -0500

    editing to find userid error

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -18,7 +18,10 @@ namespace MedTrackerScreensMVC.Data
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Dose>()
-                .HasIndex(d => new { d.MedicationId, d.Date, d.Time, d.UserId })
+                .Property(d => d.UserId)
+                .HasColumnName("UserId"); 
+            modelBuilder.Entity<Dose>()
+                .HasIndex(d => new { d.MedicationId, d.Date, d.Time, d.UserId })  
                 .IsUnique();
             modelBuilder.Entity<AuthorizedUser>()
                 .HasOne(a => a.RelationshipType)
