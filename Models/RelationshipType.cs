42d07e2f54b02fdcfb3a21212f2bc40a631e758b
Zachary Evans <zb2224@jagmail.southalabama.edu>
2026-04-15 02:32:22 -0500

    changes

--- Models/RelationshipType.cs
+++ Models/RelationshipType.cs
@@ -7,5 +7,8 @@ namespace MedTrackerScreensMVC.Models
     {
         public int Id { get; set; }
         [Required, StringLength(60)] public string Name { get; set; } = string.Empty;
+
+        [Range(1, int.MaxValue, ErrorMessage = "Please select a relationship type.")]
+        public int RelationshipTypeId { get; set; }
     }
 }
