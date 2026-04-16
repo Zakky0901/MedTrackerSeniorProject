221662452d401d09c142c4aff442b294c17b93da
Zachary Evans <zb2224@jagmail.southalabama.edu>
2026-04-16 05:21:13 -0500

    made data keys persistent

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using Auth0.AspNetCore.Authentication;
 using MedTrackerScreensMVC.Data;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -14,6 +15,20 @@ builder.Services.AddAuth0WebAppAuthentication(options =>
     options.ClientId = builder.Configuration["Auth0:ClientId"];
     options.ClientSecret = builder.Configuration["Auth0:ClientSecret"];
 });
+
+builder.Services.AddDataProtection()
+    .PersistKeysToFileSystem(new DirectoryInfo(@"C:\keys\usaran-keys\"))
+    .SetApplicationName("MedTrackerScreensMVC");
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -45,10 +60,10 @@ using (var scope = app.Services.CreateScope())
         Console.WriteLine($"Migration failed: {ex.Message}");
     }
 }
-// if (!app.Environment.IsDevelopment())
+ if (!app.Environment.IsDevelopment())
 {
-   // app.UseExceptionHandler("/Home/Error");
-    // app.UseHsts();
+   app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -56,6 +71,7 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
